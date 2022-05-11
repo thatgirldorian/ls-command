8c9130ed11ec73766195c02e58640aef9f17eb4d
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-05-12 01:37:22 +0400

    customised the dls command and made it an exec command

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+#!/usr/bin/env node
+//add a comment to run the dls command in our bin as an executable command 
+
 //require the file system module
 const fs = require('fs')
 
 //open up the current directory we're in
-fs.readdir('.', (err, filenames) => {
+fs.readdir(process.cwd(), (err, filenames) => {
     //figure out if err = error object 
     if (err) {
         //handle the error
