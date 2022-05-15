574db8d93af6156517e1d93c564e13b477c45ec5
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-05-15 16:32:17 +0400

    added a way to figure out what's a file or directory

--- index.js
+++ index.js
@@ -4,14 +4,31 @@
 //require the file system module
 const fs = require('fs')
 
+//add an lstat method
+const { lstat } = fs.promises
+
 //open up the current directory we're in
-fs.readdir(process.cwd(), (err, filenames) => {
-    //figure out if err = error object 
+fs.readdir(process.cwd(), async (err, filenames) => {
+    
     if (err) {
         //handle the error
         console.log(err)
     }
 
-    //or if there is no error
-    console.log(filenames)
+    //map over the filenames array for each filename
+    const statPromises = filenames.map(filename => {
+        return lstat(filename)
+    })
+
+    //this comprises all the stats objects available
+    const allStats = await Promise.all(statPromises)
+
+    for (let stats of allStats) {
+        const index = allStats.indexOf(stats)
+
+        //check whether the object is a file or a directory
+        console.log(filenames[index],stats.isFile())
+    }
 } )
+
+//figure out which string is a file or directory
